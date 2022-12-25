7ab99fe872e033efd172d65402ad5804c21a19b9
albanothing <92639483+albanothing@users.noreply.github.com>
2022-12-25 18:03:46 -0300

    Update composite_num_finder.py

--- composite_num_finder.py
+++ composite_num_finder.py
@@ -13,11 +13,11 @@ def AddThousandSeparators( num: int, minimum_digits: int = 6, separator: str = '
         if ( idx + 1 ) % 3 == 0: digits.append( separator )
     return num[0] + ''.join( digits[::-1] )
 
+composite_numbers = { 1: 1 } # Key = HCN, Value = number of divisors
+def_search_max    = 10_080
+upper_bound       = float( 'inf' )
+search_start      = 1
 while True:
-    composite_numbers = { 1: 1 } # Key = HCN, Value = number of divisors
-    def_search_max    = 10_080
-    infinity          = float( 'inf' )
-
     while True:
         search_max = input( 'Input largest number to search: ' ).replace(' ','')
         if search_max == '':
@@ -25,15 +25,22 @@ while True:
             print( f'Empty input. Will use default value of { AddThousandSeparators( def_search_max, 5 ) }.' )
         try:
             search_max = int( search_max )
-            if not 0 < search_max < infinity: raise
+            if not 0 < search_max < upper_bound: raise
         except: print( 'Invalid number\n' )
         else:   break
 
     print( 'Searching...' )
     start_time = pt()
-    if search_max > 1:
-        largest_divisor_count_so_far = 0
-        for num in range( 2, search_max + 1 ):
+    if search_max < search_start:
+        del_list = []
+        for key in composite_numbers:
+            if key > search_max: del_list.append( key )
+        for key in del_list:
+            del composite_numbers[key]
+        del del_list
+    elif search_max > search_start:
+        largest_divisor_count_so_far = max( composite_numbers.values() )
+        for num in range( search_start + 1, search_max + 1 ):
             idx = 2
             largest_divisor = num // 2
             divisors_count = 2 # Every number is divisible by itself and 1
@@ -43,7 +50,10 @@ while True:
             if divisors_count > largest_divisor_count_so_far:
                 largest_divisor_count_so_far = composite_numbers[num] = divisors_count
     stop_time = pt()
-    print( f'Space of highly composite numbers between 1 and { AddThousandSeparators( search_max ) } (inclusive) searched in { AddThousandSeparators( int( ( stop_time - start_time ) * 1000 ) ) } milliseconds.' )
+    if   search_max >= search_start: text_core = f'{ AddThousandSeparators( 1 if search_start == 1 else search_start + 1 ) } and { AddThousandSeparators( search_max ) } (inclusive) searched in'
+    else                           : text_core = f'{ AddThousandSeparators( search_max ) } and { AddThousandSeparators( search_start ) } wiped in'
+    print( f'Space of highly composite numbers between { text_core } { AddThousandSeparators( int( ( stop_time - start_time ) * 1000 ) ) } milliseconds.' )
+    search_start = search_max
 
     numbers              = [ str(num) for num in composite_numbers.keys() ]
     divisors             = list( composite_numbers.values() )
@@ -58,7 +68,8 @@ while True:
     plt.show           (                                                                      )
 
     while True:
-        run_again = input( 'Run again?(Y/N)\n' ).strip().lower().replace('true','True').replace('yes','True').replace('y','True').replace('false','False').replace('no','False').replace('n','False')
+        run_again = input( 'Continue searching?(Y/N)\n' ).strip().lower().replace('true','True').replace('yes','True').replace('y','True').replace('false','False').replace('no','False').replace('n','False')
+        if run_again == '': continue
         if run_again[-1] == '.': run_again = run_again[:-1]
         try:    run_again = eval( run_again )
         except: print( 'Invalid input.' )
